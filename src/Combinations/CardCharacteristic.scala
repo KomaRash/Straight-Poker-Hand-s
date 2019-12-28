1cfd41efb3bf6a349df723302ec71f58201e5523
KOma_rash <kashpirroman@gmail.com>
2019-12-28 10:53:04 +0300

    - Add ScalaDoc to project - add class uncorrected input and catch exception - Rename class Comb to Node - Rename type Dignity to Rank - add winner determination logic

--- src/Combinations/CardCharacteristic.scala
+++ src/Combinations/CardCharacteristic.scala
@@ -1,12 +1,49 @@
 package Combinations
 
+/**
+ *  object contains description  class Card and card characteristic
+ */
 package object CardCharacteristic
 {
-  type Dignity = Int
+  type Rank = Int
   type Suit = Char
+
+  /**
+   * implicit method for  cast from card to Combination:> HighCard
+   * @param card
+   * @return HighCard(card)
+   */
   implicit def toHighCard(card: Card): HighCard =HighCard(card)
 
-  case class Card(dignity: Dignity, suit: Suit) {
-    val getlowerDignity: Dignity = dignity-1
+  /**
+   * implicit Ordering for compare two cards
+   */
+  implicit val CardOrdering: Ordering[Card] = (x: Card, y: Card) => x.rank compareTo y.rank
+  /**
+   *
+   */
+  implicit val CombinationOrdering: Ordering[Combination] = (x: Combination, y: Combination) => x.powerCombination compareTo y.powerCombination
+
+
+  /**
+   *  class stores information about one card on the table
+   * @param rank - card the rank
+   * @param suit - card suit (one of "h", "d", "c", "s")
+   */
+  case class Card(rank: Rank, suit: Suit) {
+    /**
+     * value with less rank
+     */
+    val getLowerRank: Rank = rank-1
+
+    override def toString: String = s"(${rank match{
+      case 10=>"T"
+      case 11=>"J"
+      case 12=>"Q"
+      case 13=>"K"
+      case 14=>"A"
+      case rank=>rank
+    }
+    },$suit)"
   }
 }
--- src/Player.scala
+++ src/Player.scala
@@ -1,7 +1,13 @@
 import Combinations.CardCharacteristic.Card
 import Combinations.{Combination, NoCard}
 
+/**
+ * class represent player
+ * @param firstCard - first card player
+ * @param secondCard - second card player
+ */
 case class Player(firstCard:Card,secondCard:Card){
   def getStartCombination: Combination = NoCard
   def listCard():List[Card]=List(firstCard,secondCard)
+  override def toString: String = s"player with card:$secondCard;$firstCard"
 }
